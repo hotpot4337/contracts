b142d620d3ab68f1693fcd315ec0e8e13ba5896b
tomo <tomo@crowdeo.com>
2023-03-18 09:26:31 +0700

    Deploys ComplexAccount is Simple with isComplex

---
+++ deploy/3_deploy_ComplexAccountFactory.ts
@@ -0,0 +1,20 @@
+import { HardhatRuntimeEnvironment } from 'hardhat/types'
+import { DeployFunction } from 'hardhat-deploy/types'
+import { ethers } from 'hardhat'
+
+const deployComplexAccountFactory: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const provider = ethers.provider
+  const from = await provider.getSigner().getAddress()
+
+  const entrypoint = await hre.deployments.get('EntryPoint')
+  const ret = await hre.deployments.deploy(
+    'ComplexAccountFactory', {
+      from,
+      args: [entrypoint.address],
+      gasLimit: 6e6,
+      deterministicDeployment: true
+    })
+  console.log('==ComplexAccountFactory addr=', ret.address)
+}
+
+export default deployComplexAccountFactory
