7f82be1698732c0a5d24b652ffc0f36b1ca35a67
tomo <tomo@crowdeo.com>
2023-03-26 17:42:33 +0700

    Deploy onto Mantle testnet via my own Safe Singleton Factory fork of Arachnid/deterministic-deployment-proxy

--- deploy/1_deploy_entrypoint.ts
+++ deploy/1_deploy_entrypoint.ts
@@ -1,6 +1,7 @@
 import { HardhatRuntimeEnvironment } from 'hardhat/types'
 import { DeployFunction } from 'hardhat-deploy/types'
 import { Create2Factory } from '../src/Create2Factory'
+import { Create2FactoryMantle } from '../src/Create2FactoryMantle'
 import { Create2FactoryScroll } from '../src/Create2FactoryScroll'
 import { ethers } from 'hardhat'
 
@@ -9,6 +10,8 @@ const deployEntryPoint: DeployFunction = async function (hre: HardhatRuntimeEnvi
   const from = await provider.getSigner().getAddress()
   if (hre.network.name === 'scroll_alpha') {
     await new Create2FactoryScroll(ethers.provider).deployFactory()
+  } else if (hre.network.name === 'mantle_testnet') {
+    await new Create2FactoryMantle(ethers.provider).deployFactory()
   } else {
     await new Create2Factory(ethers.provider).deployFactory()
   }
